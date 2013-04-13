85cddf63eb1ea7564a6511369a3fe8309f1f3679
Bracken Mosbacker <bracken@instructure.com>
2013-04-13 17:19:09 -0600

    add outcome writeback

--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'rack-lti'
+require 'ims/lti'
 require './tool_provider'
 
 run ExampleTool
--- tool_provider.rb
+++ tool_provider.rb
@@ -1,21 +1,18 @@
 class ExampleTool < Sinatra::Base
   enable :sessions
   set :protection, :except => :frame_options
-  
+  set :session_secret, "change me please"
+
   use Rack::LTI,
       consumer_key: 'test',
       consumer_secret: 'secret',
-  
       app_path: '/launched',
       config_path: '/tool_config.xml',
       launch_path: '/lti_tool',
-  
       title: 'Example middleware LTI Tool',
       description: 'Because',
-  
       nonce_validator: ->() { true },
       time_limit: 60*60,
-  
       custom_params: {
               rack_is_great: true
       }
@@ -23,11 +20,49 @@ class ExampleTool < Sinatra::Base
   get '/' do
     erb :index
   end
-  
+
   get '/launched' do
-    # I need the launch data to do interesting stuff
-    erb :launched
+    create_tp
+
+    if @tp.outcome_service?
+      # It's a launch for grading
+      erb :assessment
+    else
+      # normal tool launch without grade write-back
+      @tp.lti_msg = "Sorry that tool was so boring"
+      erb :launched
+    end
   end
-  
-  
+
+  # post the assessment results
+  post '/assessment' do
+    create_tp
+
+    if !@tp.outcome_service?
+      return show_error "This tool wasn't lunched as an outcome service"
+    end
+
+    # post the given score to the TC
+    res = @tp.post_replace_result!(params['score'])
+
+    if res.success?
+      @score = params['score']
+      @tp.lti_msg = "Message shown when arriving back at Tool Consumer."
+      erb :assessment_finished
+    else
+      @tp.lti_errormsg = "The Tool Consumer failed to add the score."
+      show_error "Your score was not recorded: #{res.description}"
+    end
+  end
+
+  private
+
+  def create_tp
+    @tp = IMS::LTI::ToolProvider.new('test', 'secret', session['launch_params'])
+  end
+
+  def show_error(message)
+    @message = message
+    erb :error
+  end
 end
